f0e31ef6e7900b6f2c9c13eea2b8729dfed5a46f
Jai Madera <onesthathumbled@gmail.com>
2024-03-26 18:55:01 +0800

    added new func

--- reverse_string.rb
+++ reverse_string.rb
@@ -9,3 +9,15 @@ def reverse_string(string)
 end
 
 puts reverse_string('Hello')
+
+def reverse_string2(string)
+    rev_string = ""
+
+    string.each_char do |char|
+        rev_string = char + rev_string
+    end
+
+    rev_string
+end
+
+puts reverse_string2('Hello')
